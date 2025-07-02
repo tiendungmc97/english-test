2f89527d586b9c5a63fad1d83aa7bf7eb8942766
dungnguyentdt <dungnguyen5@cqtdt.asia>
2025-07-02 11:26:21 +0700

    add more sentence in reading part 3

--- src/data/reading/part-3.ts
+++ src/data/reading/part-3.ts
@@ -58,4 +58,63 @@ export const data = [
       },
     ],
   },
+  {
+    title: "Visit a City",
+    questions: [
+      {
+        id: 1,
+        question:
+          "When I first came to this city, I felt very scared because this city is very big. The city has many parts, and the shopping part is very far from the green part. However, we already had the bus route, and we signed up to take the bus for a full day. Buses are very cheap and very convenient. I feel very lucky not to have to put pressure on my sore feet.",
+        order: "A",
+      },
+      {
+        id: 2,
+        question:
+          "I don't like big cities and rarely go out. But I go to Stevenson City very often. I go there about five to seven times a year. There is an area outside the city. In that area, there are many things that I need. There is a lot of water, a natural area, a small forest, and a special camping area. I really like coming to this place to rest. It also has a great coffee serving area for visitors.",
+        order: "B",
+      },
+      {
+        id: 3,
+        question:
+          "I visit this city last summer and really liked the atmosphere of this city. The reason I like the atmosphere here is because it has a lot of public performances. These performances often take place in front of department stores, so this presents me from shopping inside those stores. I was very surprised when I came home with an empty bag, and I didn't buy anything.",
+        order: "C",
+      },
+      {
+        id: 4,
+        question:
+          "When I go out, I only care about two things: good food and good play. I especially liked the lunch I had in this city, it was delicious, and I really want to come back here next time. However, I guess I had to wait to get there. However, it didn't matter because I had a wonderful day.",
+        order: "D",
+      },
+    ],
+    listAnswers: [
+      {
+        title: "Who thinks the public transport system was good?",
+        match: "A",
+      },
+      {
+        title: "Who thinks walking too much causes a problem?",
+        match: "A",
+      },
+      {
+        title: "Who likes the natural resort here?",
+        match: "B",
+      },
+      {
+        title: "Who visits one part of the city?",
+        match: "B",
+      },
+      {
+        title: "Who likes the public theater that the city puts on?",
+        match: "C",
+      },
+      {
+        title: "Who usually spends a lot of money on shopping?",
+        match: "C",
+      },
+      {
+        title: "Who pays a lot for their meal?",
+        match: "D",
+      },
+    ],
+  },
 ];
